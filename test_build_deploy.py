a5856f547a6f8c38be97323d7bd7b01e98e97bb1
Jan-Ru Muller <janru.muller@gmail.com>
2024-05-26 19:48:32 +0200

    Expanded website menu

---
+++ test_build_deploy.py
@@ -0,0 +1,8 @@
+run tests?
+1_create_corpus.qmd
+quarto run 2_create_highlights.py
+quarto run 3_create_book.py
+move pdf from abstracts on directory up
+or move & rename pdf "1_create_corpus.pdf" one level down as ""
+check_in current version to github main branche
+publish to gh-pages, automatically after check-in?
